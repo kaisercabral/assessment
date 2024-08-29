76ad418198212895aaf6a8510ca12361d1fd6413
Arthur Kaiser <kaisercabral@gmail.com>
2024-08-29 14:14:08 +0800

    Adding comments to the classes

--- venv/tests/transactions_tests.py
+++ venv/tests/transactions_tests.py
@@ -6,11 +6,20 @@ NEW_FILE_PATH = '../files/new_transactions.csv'
 
 
 class TransactionsTests(unittest.TestCase):
+    """
+    Unit tests for the Transactions class.
+    """
 
     def test_load_file(self):
+        """
+        Tests if the specified file exists.
+        """
         self.assertTrue(os.path.isfile(FILE_PATH), f"File '{FILE_PATH}' does not exist.")
 
     def test_new_transactions_file(self):
+        """
+        Tests if a new transactions file has been created.
+        """
         self.assertTrue(os.path.isfile(NEW_FILE_PATH), f"File '{NEW_FILE_PATH}' does not exist.")
 
 
