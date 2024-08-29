5fc1fadfd266d4f6d2bdcf481b8437fa9cf7897d
Arthur Kaiser <kaisercabral@gmail.com>
2024-08-29 14:14:00 +0800

    Adding comments to the classes

--- venv/tests/labels_tests.py
+++ venv/tests/labels_tests.py
@@ -15,11 +15,20 @@ NEW_FILE_PATH = '../files/new_transactions.csv'
 
 
 class LabelTests(unittest.TestCase):
+    """
+    Unit tests for the LabelController class.
+    """
 
     def test_reading_file(self):
+        """
+        Tests if the specified file exists.
+        """
         self.assertTrue(os.path.isfile(FILE_PATH), f"File '{FILE_PATH}' does not exist.")
 
     def test_search_label(self):
+        """
+        Tests if the find_label method returns the correct label.
+        """
         labels = Util.read_labels_file(FILE_PATH)
         self.assertEqual("Clothing", LabelController.find_label(labels, "shoe"),
                              "The return is not what is expected")
